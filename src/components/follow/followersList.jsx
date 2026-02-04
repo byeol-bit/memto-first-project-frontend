fb8d1159357fff4f3e2e77153084f91028d6df5c
yang-hee <yangheejin746@gmail.com>
2026-02-04 19:26:38 +0900

    🎨 Style : 팔로우 리스트 페이지 css 적용 - 이미지, 이름, 등급, 팔로우 버튼 출력

--- src/components/follow/followersList.jsx
+++ src/components/follow/followersList.jsx
@@ -1,9 +1,30 @@
-import { useParams } from "react-router"
 import FollowUserCard from "./followUserCard"
 
+import { followers } from "../../data/users.mock"
+
 const FollowersList = ({userId}) => {
+    // const [users, setUsers] = useState([])
+    // 유저리스트 불러오기
+    // useEffect() => {
+
+    // }
+
+    // 팔로우 버튼 눌렀을 때 (상태가 팔로잉이면? 언팔로우 / 팔로잉중이지 않으면? 팔로우) 상태변경! 리스트는 유지
+    // const toggleFollow = 
+
     return (
         <div>
+            {followers.map((user) => {
+                return (
+                    <FollowUserCard 
+                        key = {user.id}
+                        user={user}
+                        // 유저의 상태에 따라 출력되도록 수정하기 (팔로우 / 팔로잉)
+                        type={'팔로우'}
+                        toggleFollow={() => alert('팔로우')}    
+                    />
+                )
+            })}
             <>팔로워 리스트 {userId}</>
         </div>
     )
--- src/components/follow/followingsList.jsx
+++ src/components/follow/followingsList.jsx
@@ -1,13 +1,25 @@
-import { useNavigate, useParams } from "react-router"
+import { followers } from "../../data/users.mock"
+import FollowUserCard from "./followUserCard"
 
+// 팔로잉이랑 팔로워랑 다른 데이터 사용하기 때문에 각각의 리스트 창에서 데이터 패칭
 const FollowingsList = ({userId}) => {
-    const navigate = useNavigate()
+
+    
 
     return (
         <div>
-            <div onClick={() => naviagate(`/users`)}></div>
-
-            팔로잉 리스트 {userId}
+            {followers.map((user) => {
+                return (
+                    <FollowUserCard 
+                        key = {user.id}
+                        user={user}
+                        // 유저의 상태에 따라 출력되도록 수정하기 (팔로우 / 팔로잉)
+                        type={'팔로우'}
+                        toggleFollow={() => alert('팔로우')}    
+                    />
+                )
+            })}
+        
         </div>
     )
 }
