b6b38a6303bd8807898d880e7e960be592ce5898
youngsik <syssys6831@gmail.com>
2026-02-10 15:46:17 +0900

    🐛 Fix : axios, auth import, export 구문 수정, proxy 사용 (#32)
    * ✨Feature/sign up order&logic
    
    * 🐛 Fix : axios, auth import, export 구문 수정
    
    * 🐛 Fix : proxy 사용해서 연결문제 해결
    
    * 🐛 Fix : proxy 사용안하고도 연결문제 해결 (withCredentials: true 제거)

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,20 +1,13 @@
 import axios from "axios";
 
+// 프록시를 사용할 때는 baseURL을 반드시 빈 문자열로 설정해야 합니다.
+// 그래야 요청이 http://localhost:5173/users/... 로 날아가고,
+// Vite가 그걸 낚아채서 fly.dev로 보내줍니다.
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
-  headers: { "Content-Type": "application/json" },
-  // 쿠키나 인증 헤더 정보 포함시켜 요청하고 싶을 때 withCredentials: true
+  baseURL: "https://hidden-master-server.fly.dev",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-// 응답
-api.interceptors.response.use(
-  (response) => response.data,
-  (error) => {
-    const status = error.response?.status;
-    if (status === 401) {
-      console.log("인증 실패: 로그인 필요");
-    }
-    return Promise.reject(error);
-  },
-);
 export default api;
