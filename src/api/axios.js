da24981f795e5b834c7b0774938aedcd65abcf35
youngsik <syssys6831@gmail.com>
2026-02-25 15:10:47 +0900

    ✨ feat : 토큰 만료 후 api 호출시 로그아웃 처리

--- src/api/axios.js
+++ src/api/axios.js
@@ -8,4 +8,21 @@ const api = axios.create({
   withCredentials: true,
 });
 
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      console.warn("로그인 세션이 만료되었습니다.");
+
+      localStorage.clear();
+
+      alert("로그인 세션이 만료되었습니다. 다시 로그인해 주세요.");
+      window.location.href = "/sign-in";
+    }
+
+    return Promise.reject(error);
+  },
+);
 export default api;
