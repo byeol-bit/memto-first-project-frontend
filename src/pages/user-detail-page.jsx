a989671c272d6076d8e514a7770a42f097801ee1
yang-hee <yangheejin746@gmail.com>
2026-02-25 13:09:22 +0900

    ✨feat: 유저 디테일 페이지 팔로우 추가

--- src/pages/user-detail-page.jsx
+++ src/pages/user-detail-page.jsx
@@ -1,31 +1,19 @@
-import { useParams, useNavigate } from "react-router"
 import { useState, useContext, useRef } from "react"
 
 import { DetailStateContext } from "../components/layout/map-layout"
 
-import TabButton from "../components/tabButton"
 import FollowButton from "../components/user-detail-components/followButton"
 import UserProfile from "../components/user-detail-components/userProfile"
 import UserReview from "../components/user-detail-components/userReview"
 import LoginTooltip from "../components/loginTooltip"
+import Loading from "../components/loading"
 
 import { useUserDetail, useIsFollowing, useCountFollowing, useCountFollower } from "../hooks/queries/use-users-data"
-import {useFollow, useUnfollow} from "../hooks/mutations/use-create-user-mutation"
-
-
-const TABS = {
-  REVIEWS: 'reviews',
-  FOLLOWERS: 'followers',
-  FOLLOWINGS: 'followings'
-}
-
+import { useToggleFollow } from "../hooks/mutations/use-create-user-mutation"
 
 const UserDetailPage = () => {
   const context = useContext(DetailStateContext)
-  const navigate = useNavigate()
 
-  const {id} = useParams()
-  const [selectedTab, setSelectedTab] = useState(TABS.REVIEWS)
   const [showTooltip, setShowTooltip] = useState(false);
   const tooltipTimer = useRef(null)
 
@@ -33,17 +21,17 @@ const UserDetailPage = () => {
   const isLogin = true
 
   let currentId = context?.selectedUser?.id
-    ? parseInt(context.selectedUser.id)
-    : parseInt(id) 
+
+  if(!currentId) {
+    return null
+  }
 
   const { data: user, isLoading, error} = useUserDetail(currentId)
   const { data: isFollowing } = useIsFollowing(currentId)
   const { data: followingsCount } = useCountFollowing(currentId)
   const { data: followersCount } = useCountFollower(currentId)
 
-  const followMutation = useFollow(currentId)
-  const unfollowMutation = useUnfollow(currentId)
-  
+  const { mutate: toggleFollow } = useToggleFollow()
 
   const requireLogin = (callback) => {
     if(!isLogin) {
@@ -59,60 +47,50 @@ const UserDetailPage = () => {
 
   const handleFollowToggle = () => {
     requireLogin(() => {
-      if(isFollowing) {
-        unfollowMutation.mutate()
-      } else {
-        followMutation.mutate()
-      }
+      toggleFollow({
+        userId: currentId,
+        isFollowing: isFollowing
+      })
     })
   }
 
   // 내가 보고 있는 페이지 기억하기..
   const handleLoginRedirect = () => {
-  //   sessionStorage.setItem(
-  //     'returnDetailState',
-  //     JSON.stringify({
-  //       userId: currentId,
-  //     })
-  //   )
-  //   navigate("/sign-up")
+
   }
   
-  if(isLoading) return <div>유저 정보를 불러오고 있습니다.</div>
+  if(isLoading) return <Loading />
   if(error) return <div>유저 정보를 불러오는데 실패했습니다.</div>
 
   return (
     <div>
-      <UserProfile user={user} />
+      <UserProfile user={user}/>
       {/* 팔로잉 수 / 팔로워 수 */}
-      <div className="flex w-full py-4 border-b border-gray-100">
-
+      <div className="flex w-full mt-3 border-b border-t border-gray-50">
         {/* 팔로워 */}
         <div
-          onClick={() => 
-            requireLogin(() => 
-              navigate(`/users/${currentId}/followers`)
-          )}
-          className="flex-1 flex flex-col items-center cursor-pointer hover:text-red-400 hover:bg-gray-100"
+          onClick={() => {
+            context.setUserDetailView("followers")
+          }}
+          className="flex-1 flex flex-col py-1.5 items-center cursor-pointer hover:text-red-400 hover:bg-gray-100"
         >
           <span className="text-lg font-semibold">
-            {followersCount?.count ?? 0}
+            {followersCount ?? 0}
           </span>
           <span className="text-sm text-gray-500">
             팔로워
           </span>
         </div>
-
+        <div className="w-px self-stretch bg-gray-50"/>
         {/* 팔로잉 */}
         <div
           onClick={() => 
-            requireLogin(() =>
-              navigate(`/users/${currentId}/followings`)
-          )}
-          className="flex-1 flex flex-col items-center cursor-pointer  hover:text-red-400 hover:bg-gray-100"
+            context.setUserDetailView("followings")
+          }
+          className="flex-1 flex flex-col py-1.5 items-center cursor-pointer  hover:text-red-400 hover:bg-gray-100"
         >
           <span className="text-lg font-semibold">
-            {followingsCount?.count ?? 0}
+            {followingsCount ?? 0}
           </span>
           <span className="text-sm text-gray-500">
             팔로잉
@@ -121,7 +99,7 @@ const UserDetailPage = () => {
         
       </div>
       <div className="relative flex justify-center mt-4">
-        <FollowButton isFollowing={isFollowing} onToggle={handleFollowToggle}/>
+        <FollowButton isFollowing={!!isFollowing} onToggle={handleFollowToggle}/>
         {showTooltip && (
           <LoginTooltip 
             onClick={handleLoginRedirect}
@@ -131,12 +109,8 @@ const UserDetailPage = () => {
           />
         )}
       </div>
-      <div className="flex border-b border-b-gray-200 mt-2">
-        <TabButton
-          label="리뷰"
-          active={selectedTab === TABS.REVIEWS}
-          onClick={() => setSelectedTab(TABS.REVIEWS)}
-        />
+      <div className="flex border-b border-b-gray-200 mt-4">
+        <div className="text-center flex-1 py-4 text-sm font-medium border-t-2 border-red-400 text-red-400">리뷰</div>
       </div>
       <div>
         <UserReview userId={currentId}/>
