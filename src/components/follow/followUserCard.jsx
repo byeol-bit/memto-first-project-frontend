a536b6ad99f115117df5a11a46851c5e496b85e7
yang-hee <yangheejin746@gmail.com>
2026-02-26 16:20:22 +0900

    ✨ Feat : 유저 이미지 적용 - 로그인 안한 상태에서 팔로우 버튼 선택 시 로그인 페이지로 이동 - userCard / followUserCard css 수정

--- src/components/follow/followUserCard.jsx
+++ src/components/follow/followUserCard.jsx
@@ -1,36 +1,27 @@
-import {profileImage} from '../../data/users.mock'
 import FollowButton from "./followButton"
 import { useToggleFollow } from "../../hooks/mutations/use-create-user-mutation"
-import { useState, useContext, useRef } from "react"
+import { useContext } from "react"
 import { DetailStateContext } from "../layout/map-layout"
 import { useLoginState } from '../loginstate'
-import LoginTooltip from '../loginTooltip'
+import { useNavigate } from 'react-router'
+import { userImg } from '../../api/user.api'
+import { useCountFollower } from '../../hooks/queries/use-users-data'
 
 const FollowUserCard = ({user, isFollowing}) => {
     const context = useContext(DetailStateContext)
+    const navigate = useNavigate()
     const {isLoggedIn, user:isMe} = useLoginState()
     const {mutate: toggleFollow} = useToggleFollow()
-    
-    const [showTooltip, setShowTooltip] = useState(false)
-    const [tooltipPosition, setTooltipPosition] = useState(null)
-    const tooltipTimer = useRef(null)
-
+    const {data: followerCount} = useCountFollower(user.id)
+    // const {data: }
 
     const handleFollowToggle = (e) => {
     	if (!isLoggedIn) {
-      		const rect = e.currentTarget.getBoundingClientRect()
-		    setTooltipPosition({
-        	top: rect.bottom + 8,
-        	left: rect.left + rect.width / 2 
-      	})
+            alert("로그인이 필요합니다.")
+            navigate('/sign-in')
+            return;
+      	}
       
-      	setShowTooltip(true)
-      	clearTimeout(tooltipTimer.current)
-      	tooltipTimer.current = setTimeout(() => {
-        	setShowTooltip(false)
-      	}, 2000)
-      	return
-    }
     	toggleFollow({
       		userId: user.id,
       		isFollowing: isFollowing
@@ -40,45 +31,40 @@ const FollowUserCard = ({user, isFollowing}) => {
 		context.setSelectedUser(selectedUser)
 		context.setUserDetailView("detail")
 	}
-
-    const handleLoginRedirect = () => {
-    	navigate("/sign-in", {
-      	state: {
-        from: {
-          selectedUser: context.selectedUser,
-          userDetailView: context.userDetailView
-        }
-      }
-    })
-    	return
-  	}
-	console.log('내가 누구인가!!', isMe, user.id)
+    console.log(user, userImg(user.id), userImg)
     return (
         // 유저 카드 누르면 해당 유저 상세페이지로 
         <div 
             className='flex items-center justify-between max-w-3xl mx-auto px-6 py-4 border-y cursor-pointer border-gray-100 hover:bg-gray-50' 
             onClick={() => handleCardClick(user)}
         >
-            <div className='flex items-center gap-4'>
-                <img className="rounded-full w-16 h-16 object-cover shrink-0" src={profileImage}/>
-                <div className='font-semibold text-gray-900'>
-                    {user.nickname}
-                </div>
-                <div className='px-2 inline-block border-red-400 rounded-full border text-xs text-red-400 w-fit'>
-                    {user.category}
+            <div>
+                <div className='flex items-center gap-4'>
+                    <img className="rounded-full w-16 h-16 object-cover shrink-0 shadow-md" src={userImg(user.id)}/>
+                    <div className="flex flex-col justify-center">
+                        <div className="flex items-center gap-2">
+                            <span className="font-semibold text-gray-900">
+                                {user.nickname}
+                            </span>
+
+                            <span className='px-2 border border-red-400 rounded-full text-xs text-red-400 w-fit'>
+                                {user.category}
+                            </span>
+                        </div>
+                        <div className='flex items-center gap-4 mt-1 text-sm text-gray-400'>
+                            <span>
+                                리뷰 <span className="font-medium">{user.visitCount? user.visitCount : 0}</span>
+                            </span>
+                            <span>
+                                팔로워 <span classname="font-medium">{followerCount}</span>
+                            </span>
+                        </div>
+                    </div>
                 </div>
             </div>
+
             {user?.id !== isMe?.id && (
               <FollowButton isFollowing={isFollowing} onToggle={handleFollowToggle} />
-            )}
-          {showTooltip && (
-              <LoginTooltip
-                  position={tooltipPosition}
-                  onClick={handleLoginRedirect}
-                  onClose={() => setShowTooltip(false)}
-                  mainText='로그인이 필요합니다'
-                  buttonText='로그인'
-              />
             )}
         </div>
     )
